a5d7f574c37eb9619bbc977f2a410c2c6cbdafa4
hmimou <hmimounada1@gmail.com>
2022-04-10 01:12:08 +0000

    initial commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,173 @@
+<template>
+  <div>
+    <div class="body"></div>
+    <div class="grad"></div>
+    <div class="header">
+      <div>
+        <img
+          alt="alliancelogo"
+          src="../assets/images/TangerAlliance-logo.png"
+        />
+      </div>
+    </div>
+    <br />
+    <div class="login">
+      <input type="text" placeholder="username" name="user" /><br />
+      <input type="password" placeholder="password" name="password" /><br />
+      <input type="button" value="Login" />
+      <p class="text-muted text-center mt-3 mb-0">
+        Don't have an account?
+        <router-link to="/register"
+          ><a href="/register" class="text-info ml-1">register</a></router-link
+        >
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginView",
+};
+</script>
+
+<style scoped>
+a {
+  color: #2e5cb8;
+}
+body {
+  margin: 0;
+  padding: 0;
+  background: #fff;
+
+  color: #fff;
+  font-family: Arial;
+  font-size: 12px;
+}
+
+.body {
+  position: absolute;
+  top: -20px;
+  left: -20px;
+  right: -40px;
+  bottom: -40px;
+  width: auto;
+  height: auto;
+  background-image: url("../assets/images/loginbg.jpg");
+  background-size: cover;
+  z-index: 0;
+}
+
+.grad {
+  position: absolute;
+  top: -20px;
+  left: -20px;
+  right: -40px;
+  bottom: -40px;
+  width: auto;
+  height: auto;
+  z-index: 1;
+  opacity: 0.7;
+}
+
+.header {
+  position: absolute;
+  top: calc(50% - 70px);
+  left: calc(50% - 265px);
+  z-index: 2;
+}
+
+.header div {
+  float: left;
+  color: #fff;
+  font-family: "Exo", sans-serif;
+  font-size: 35px;
+  font-weight: 200;
+}
+
+.header div span {
+  color: #5379fa !important;
+}
+
+.login {
+  position: absolute;
+  top: calc(50% - 75px);
+  left: calc(50% - 50px);
+  height: 150px;
+  width: 350px;
+  padding: 10px;
+  z-index: 2;
+}
+
+.login input[type="text"] {
+  width: 250px;
+  height: 30px;
+  background: transparent;
+  border: 1px solid rgba(255, 255, 255, 0.6);
+  border-radius: 2px;
+  color: #fff;
+  font-family: "Exo", sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  padding: 4px;
+}
+
+.login input[type="password"] {
+  width: 250px;
+  height: 30px;
+  background: transparent;
+  border: 1px solid rgba(255, 255, 255, 0.6);
+  border-radius: 2px;
+  color: #fff;
+  font-family: "Exo", sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  padding: 4px;
+  margin-top: 10px;
+}
+
+.login input[type="button"] {
+  width: 260px;
+  height: 35px;
+  background: #fff;
+  border: 1px solid #fff;
+  cursor: pointer;
+  border-radius: 2px;
+  color: #a18d6c;
+  font-family: "Exo", sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  padding: 6px;
+  margin-top: 10px;
+}
+
+.login input[type="button"]:hover {
+  opacity: 0.8;
+}
+
+.login input[type="button"]:active {
+  opacity: 0.6;
+}
+
+.login input[type="text"]:focus {
+  outline: none;
+  border: 1px solid rgba(255, 255, 255, 0.9);
+}
+
+.login input[type="password"]:focus {
+  outline: none;
+  border: 1px solid rgba(255, 255, 255, 0.9);
+}
+
+.login input[type="button"]:focus {
+  outline: none;
+}
+
+::-webkit-input-placeholder {
+  color: rgba(255, 255, 255, 0.6);
+}
+
+::-moz-input-placeholder {
+  color: rgba(255, 255, 255, 0.6);
+}
+</style>
---
+++ src/views/RegisterUserView.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div class="body"></div>
+    <div class="login-form">
+      <h4>Register to Administration</h4>
+      <form>
+        <div class="form-group">
+          <label>User Name</label>
+          <input type="email" class="form-control" placeholder="User Name" />
+        </div>
+        <div class="form-group">
+          <label>Email address</label>
+          <input type="email" class="form-control" placeholder="Email" />
+        </div>
+        <div class="form-group">
+          <label>Password</label>
+          <input type="password" class="form-control" placeholder="Password" />
+        </div>
+        <div class="checkbox">
+          <label> <input type="checkbox" /> Agree the terms and policy </label>
+        </div>
+        <button type="submit" class="btn btn-primary btn-flat m-b-30 m-t-30">
+          Register
+        </button>
+        <div class="social-login-content">
+          <div class="social-button">
+            <button
+              type="button"
+              class="btn btn-primary bg-facebook btn-flat btn-addon m-b-10"
+            >
+              <i class="ti-facebook"></i>Register with facebook
+            </button>
+            <button
+              type="button"
+              class="btn btn-primary bg-twitter btn-flat btn-addon m-t-10"
+            >
+              <i class="ti-twitter"></i>Register with twitter
+            </button>
+          </div>
+        </div>
+        <div class="register-link m-t-15 text-center">
+          <p>Already have account ? <a href="#"> Sign in</a></p>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RegisterUserView",
+};
+</script>
+
+<style scoped>
+.body {
+  position: absolute;
+  top: -20px;
+  left: -20px;
+  right: -40px;
+  bottom: -40px;
+  width: auto;
+  height: auto;
+  background-image: url("../assets/images/loginbg.jpg");
+  background-size: cover;
+  z-index: 0;
+}
+</style>
